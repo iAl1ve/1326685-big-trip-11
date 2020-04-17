2b645b7b28142ae69e3b2e68ce836fb99b0a3eb1
Виталий Москвин <moskvin.vitaly@gmail.com>
2020-04-17 17:28:20 +0300

    Личный проект: революция или эволюция? (Часть 2)

--- src/components/cost.js
+++ src/components/cost.js
@@ -2,7 +2,7 @@ import {createElement} from "../utils.js";
 
 const createTripCostTemplate = (cost) => {
   return (
-    `<p class="trip-info__cost">Total: &euro;&nbsp;<span class="trip-info__cost-value">${cost ? cost: 0}</span></p>`
+    `<p class="trip-info__cost">Total: &euro;&nbsp;<span class="trip-info__cost-value">${cost ? cost : 0}</span></p>`
   );
 };
 
